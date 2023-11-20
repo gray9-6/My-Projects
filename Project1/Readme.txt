0ec61e5009d286cbf1739a111775344cc0eb6670
Ajay Kumar <aky22896@gmail.com>
2023-11-20 17:28:04 +0530

    my first project

---
+++ Project1/Readme.txt
@@ -0,0 +1,14 @@
+Title :- 
+        Qurious chrome extensions
+
+
+Summary :- 
+
+1. An assistanr in the form of a chrome extension to browse internet efficiently.
+2. It should handle audio input.
+3. It should be able to take non - precise command to do useful output
+   
+
+
+
+chrome://extensions/
---
+++ Project1/version1/scripts/content.js
@@ -0,0 +1,23 @@
+let body = document.querySelector("body");
+
+let btnQurious = document.createElement("button");
+btnQurious.setAttribute("id","btnQurious");
+btnQurious.addEventListener("click",doSomething);
+body.appendChild(btnQurious);
+
+
+let speechRecognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+speechRecognition.
+
+
+function doSomething(){
+    if(btnQurious.hasAttribute("listening") === false){
+        btnQurious.setAttribute("listening",true);
+        console.log("I started listening");
+    }else{
+        btnQurious.removeAttribute("listening");
+        console.log("I stopped listening");
+    }
+}
+
